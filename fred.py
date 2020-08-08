104b5b6b7260b6eba05f52368c1369aa479d583b
Andrew Lamont <andrew.m.lamont@gmail.com>
2020-08-08 12:41:41 -0400

    initializing

---
+++ fred.py
@@ -0,0 +1,93 @@
+# Andrew Lamont
+# August 2020
+# This script runs FRed on a basis passed in as a tsv
+
+import sys
+
+# point-wise fusion
+def fuse(a, b):
+	if a == 'L' or b == 'L': return 'L'
+	if a == 'e': return b
+	if b == 'e': return a
+	return 'W'
+
+# FRed
+def FRed(comptableau):
+	FNF = set([])
+
+	# if A is empty, then FNF is empty
+	if not comptableau:
+		return set([])
+
+	cols = len(comptableau[0])
+
+	# Fuse the entire tableau
+	fuseall = ['e'] * cols
+
+	# Residuals - collect vectors with an 'e' in col i
+	res = []
+	for c in range(cols):
+		res.append([])
+
+	for row in comptableau:
+		fuseall = [fuse(a,b) for a,b in zip(fuseall, row)]
+		for c in range(cols):
+			if row[c] == 'e':
+				res[c].append(row)
+
+	holdfus = [tuple(fuseall)]
+
+	# Update residuals - keep only cols with 'W' in fuseall
+	for c in range(cols):
+		if fuseall[c] != 'W':
+			res[c] = []
+
+	# Check entailments
+
+	# if fuseall is all 'W', then no ranking info
+	if 'L' not in fuseall and 'e' not in fuseall:
+		holdfus = []
+
+	# if fuseall is all 'L', then unsatisfiable
+	if 'W' not in fuseall:
+		return ('unsat',)
+
+	# does the set of residuals entail fuseall?
+
+	# fuse residuals
+	fuseres = ['e'] * cols
+	for c in range(cols):
+		for r in res[c]:
+			fuseres = [fuse(a,b) for a,b in zip(fuseres, r)]
+
+	# fuseres entails fuseall if they have the same number of 'L's
+	if fuseres.count('L') == fuseall.count('L'):
+		holdfus = []
+
+	if holdfus:
+		FNF.add(tuple(fuseall))
+
+	# recurse on the residuals
+	for c in range(cols):
+		FNF = FNF.union(FRed(res[c]))
+
+	if ('unsat',) in FNF:
+		return ('unsat',)
+
+	return FNF
+
+basisfile = open(sys.argv[1])
+basis = []
+
+# skip first line
+basisfile.readline()
+
+for line in basisfile:
+	if not line.strip(): continue
+	# ignore first column
+	linspl = line.strip().split('\t')[1:]
+	basis.append(linspl)
+
+fnf = FRed(basis)
+for erc in fnf:
+	print(erc)
